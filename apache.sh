8e800b64743b385e9bd5323cca420e9058cec401
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2021-05-12 21:45:07 +0100

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -3,7 +3,7 @@
 container=${buildah from ubuntu:latest}
 buildah config --label maintainer="dfoley" $container
 
-buildah run $cotainer apt install apache2
+buildah run $container apt install apache2
 
 buildah config --port 80 $container
 buildah commit --format docker $container test:latest
