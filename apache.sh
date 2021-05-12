c38bed30471f08d6653c1159cc80bcfb77356d83
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2021-05-12 21:45:57 +0100

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -7,3 +7,5 @@ buildah run $container apt install apache2
 
 buildah config --port 80 $container
 buildah commit --format docker $container test:latest
+
+podman run -d --name testApache -p 8081:80 localhost/test
